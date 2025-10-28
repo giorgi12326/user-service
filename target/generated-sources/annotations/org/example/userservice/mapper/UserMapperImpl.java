7e9887178f4d2b37943f3ad198d1ffce2012065c
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-10-28 14:30:48 +0400

    ci-cd

--- target/generated-sources/annotations/org/example/userservice/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/org/example/userservice/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-10-21T16:33:55+0400",
+    date = "2025-10-28T13:07:08+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Eclipse Adoptium)"
 )
 @Component
