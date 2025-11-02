282718c5d07614dc1e65620019974c95b477475a
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-02 14:53:36 +0400

    temp disble security

--- src/main/java/org/example/userservice/security/SecurityConfig.java
+++ src/main/java/org/example/userservice/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                     request.requestMatchers("/**").permitAll()
 
             )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         ;
 
         return http.build();
