609e2e1280559c2014aae57b4610ad29464f1c88
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-03 13:42:51 +0400

    added fialing test frfr2

--- src/test/java/org/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/org/example/userservice/UserServiceApplicationTests.java
@@ -3,11 +3,19 @@ package org.example.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void makeFail(){
+        int a = 1;
+        assertEquals(3,a);
     }
 
 }
