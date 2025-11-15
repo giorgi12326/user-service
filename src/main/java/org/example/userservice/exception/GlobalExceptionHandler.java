bf3e2839d653b7e320994d55a74c7b1f835e1a6e
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-15 08:42:54 +0400

    changed to rest :)

--- src/main/java/org/example/userservice/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/userservice/exception/GlobalExceptionHandler.java
@@ -5,10 +5,12 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
